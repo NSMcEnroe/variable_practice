993dad6522a0898a73234faa215bc1c664574e44
Nicholas McEnroe <nsmcenro@gmail.com>
2023-05-31 14:52:06 -0400

    Add float variables

--- variables.rb
+++ variables.rb
@@ -4,9 +4,15 @@ name = "Nick"
 current_state = "Michigan"
 footwear = "sandals"
 
-#  Integer Variables
+# Integer Variables
 
 age = "26"
 dogs = "2"
 bikes = "2"
+
+# Float Variables
+
+bank_account = "0.01"
+eye_prescription = "0.5"
+current_rent = "1770.00"
 
