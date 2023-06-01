3d5ad0785c312ab992352f93c5f330188ca8bea3
Nicholas McEnroe <nsmcenro@gmail.com>
2023-06-01 14:07:24 -0400

    Fix integer and float variables

--- variables.rb
+++ variables.rb
@@ -6,15 +6,15 @@ footwear = "sandals"
 
 # Integer Variables
 
-age = "26"
-dogs = "2"
-bikes = "2"
+age = 26
+dogs = 2
+bikes = 2
 
 # Float Variables
 
-bank_account = "0.01"
-eye_prescription = "0.5"
-current_rent = "1770.00"
+bank_account = 0.01
+eye_prescription = 0.5
+current_rent = 1770.00
 
 # Boolean Variables
 
